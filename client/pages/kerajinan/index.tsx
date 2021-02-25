498fb7dcd456eeeb8293cbd34bc7588ac2a0a3f1
unknown <azharalifauzi@gmail.com>
2021-02-25 11:31:08 +0700

    kerajinan page responsive

--- client/pages/kerajinan/index.tsx
+++ client/pages/kerajinan/index.tsx
@@ -20,17 +20,17 @@ const KerajinanPage = () => {
     <>
       <Header />
       <section className="container mx-auto px-10">
-        <h2 className="text-center font-medium text-h2 mt-48 mb-7">Kerajinan</h2>
+        <h2 className="text-center font-medium text-h4 lg:text-h2 mt-48 mb-7">Kerajinan</h2>
       </section>
-      <section className="container mx-auto mb-16 px-10">
+      <section className="container mx-auto mb-16 px-6 lg:px-10">
         <div className="flex items-center">
-          <Button customHeight className="mr-11 h-8">
+          <Button customHeight className="lg:mr-11 mr-3 h-8">
             Filter
           </Button>
-          <TextField variant="search-right" />
+          <TextField inputClassName="w-full" className="w-full lg:w-auto" variant="search-right" />
         </div>
       </section>
-      <section className="container mx-auto mb-16 px-10">
+      <section className="container mx-auto mb-16 px-6 lg:px-10">
         <div
           style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))' }}
           className="grid gap-x-12 gap-y-10"
