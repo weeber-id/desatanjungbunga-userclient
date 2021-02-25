4dea012ecc8591782631e5776a1f48657dc48d79
unknown <azharalifauzi@gmail.com>
2021-02-25 12:36:01 +0700

    kerajinan detail page responsive

--- client/pages/kerajinan/[slug].tsx
+++ client/pages/kerajinan/[slug].tsx
@@ -12,34 +12,40 @@ const KerajinanDetailPage = () => {
     <>
       <Header />
       <section style={{ paddingTop: 38 * 4 }} className="bg-blue-light mb-16">
-        <div className="container mx-auto px-10 flex justify-end pb-4">
+        <div className="container mx-auto px-6 md:px-10 flex justify-end pb-4">
           <BreadCrumb>
             <BreadCrumbItem href="/kerajinan">Kerajinan</BreadCrumbItem>
             <BreadCrumbItem isActive>{slug}</BreadCrumbItem>
           </BreadCrumb>
         </div>
       </section>
-      <section className="container mx-auto px-10 mb-16">
-        <div className="grid grid-cols-2 gap-x-16">
+      <section className="container mx-auto px-6 md:px-10 mb-16">
+        <div className="grid grid-cols-1 gap-y-4 md:grid-cols-2 gap-x-16">
           <div>
             <div className="sticky top-24">
               <Image layout="responsive" width={1200} height={900} src={DummyMasakan} />
             </div>
           </div>
           <div>
-            <h3 className="text-h3 font-medium text-black mb-3">Topi Benjang</h3>
-            <p className="text-body text-purple-light mb-3 font-bold">Harga mulai:</p>
-            <h4 className="text-h4 text-black font-medium mb-1">Rp 10,000</h4>
-            <p className="text-body text-black mb-8">Buka Senin - Jumat (08.00 - 17.00)</p>
-            <p className="text-body text-purple-light mb-3 font-bold">
+            <h3 className="md:text-h3 text-h5 font-medium text-black mb-3">Topi Benjang</h3>
+            <p className="md:text-body text-body-sm text-purple-light mb-3 font-bold">
+              Harga mulai:
+            </p>
+            <h4 className="md:text-h4 text-h5 text-black font-medium mb-1">Rp 10,000</h4>
+            <p className="md:text-body text-body-sm text-black mb-8">
+              Buka Senin - Jumat (08.00 - 17.00)
+            </p>
+            <p className="md:text-body text-body-sm text-purple-light mb-3 font-bold">
               Sekilas Tentang Topi Benjang :
             </p>
-            <p className="text-body text-black mb-6">
+            <p className="md:text-body text-body-sm text-black mb-6">
               Phasellus varius volutpat tellus ac sollicitudin. Suspendisse tempor ligula vitae
               tempor egestas. Nulla pharetra felis, A pretium vulputate. Nunc gravida lectus sapien.
               Dui tempor.
             </p>
-            <p className="text-body text-purple-light mb-5 font-bold">Pesan Sekarang :</p>
+            <p className="md:text-body text-body-sm text-purple-light mb-5 font-bold">
+              Pesan Sekarang :
+            </p>
             <div className="flex items-center mb-12">
               <Button className="mr-6">Tokopedia</Button>
               <Button>Bukalapak</Button>
