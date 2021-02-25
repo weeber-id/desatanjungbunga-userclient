23c32a7aa593bdbbb9674f95a4df437d97967f7d
unknown <azharalifauzi@gmail.com>
2021-02-25 14:02:53 +0700

    footer responsive

--- client/components/organisms/footer/index.tsx
+++ client/components/organisms/footer/index.tsx
@@ -4,23 +4,27 @@ import { Button } from '../../atoms';
 const Footer = () => {
   return (
     <footer className="bg-blue-light pt-20 pb-14">
-      <div className="container px-10 mx-auto grid grid-cols-2">
+      <div className="container px-6 md:px-10 mx-auto grid grid-cols-1 gap-y-4 md:grid-cols-2">
         <div>
-          <IconTanjungBunga className="mb-9" width={280} height={95.6} viewBox="0 0 174 64" />
-          <h4 className="font-medium text-h4 text-purple-light mb-1">Alamat</h4>
-          <p className="text-body text-purple-light">
+          <IconTanjungBunga className="mb-9 w-48 h-16 md:w-72 md:h-24" viewBox="0 0 174 64" />
+          <h4 className="font-medium md:text-h4 text-body text-purple-light mb-1">Alamat</h4>
+          <p className="md:text-body text-body-sm text-purple-light">
             Desa Tanjung Bunga, Kecamatan Pangururan, <br />
             Kabupaten Samosir, Provinsi Sumatra Utara, <br />
             Indonesia, Kode Pos 22392.
           </p>
         </div>
         <div>
-          <h3 className="text-h3 text-purple-light font-medium mb-1.5">Hubungi Kami</h3>
-          <h3 className="text-h3 text-purple-light font-medium mb-2.5">0626 - 01020304</h3>
+          <h3 className="md:text-h3 text-body text-purple-light font-medium mb-1.5">
+            Hubungi Kami
+          </h3>
+          <h3 className="md:text-h3 text-h5 text-purple-light font-medium mb-2.5">
+            0626 - 01020304
+          </h3>
           <Button variant="outlined" color="red">
             Lihat Lengkap
           </Button>
-          <p className="text-body-sm text-purple-light mt-6 mb-2">Follow us:</p>
+          <p className="text-body-sm text-purple-light mt-3 md:mt-6 mb-2">Follow us:</p>
           <div className="flex items-center">
             <a
               href="http://facebook.com"
